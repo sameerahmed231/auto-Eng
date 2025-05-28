1ba9c5de08c3fb0e3e016c6c84394dc9a5bed148
sameerahmed231 <ksam2251@gmail.com>
2025-05-29 01:35:23 +0500

    complete challange 2

---
+++ Chapters/Ch1/Challenge2.py
@@ -0,0 +1,7 @@
+season = input('Enter Season of the year ')
+
+number = int(input('Enter a Number'))
+
+adjective = input('Enter an Adjective')
+
+print(f'On a {adjective} {season} day. I drink {number} cups of juice')
